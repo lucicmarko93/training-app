56e6ed6b8d5beee323ad70eb55b2535a90194f35
lucicmarko93 <marko.lucic@netsetglobal.rs>
2020-05-10 20:11:47 +0200

    first servlet

---
+++ training-app-web/src/main/java/org/training/app/web/FirstServlet.java
@@ -0,0 +1,58 @@
+package org.training.app.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SearchFacadeClient</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>test ... </h1>");
+                       
+            System.out.println("Printing Shopper Count after incrementing it ...");
+            
+            out.println("</body>");
+            out.println("</html>");
+        } finally {            
+            out.close();
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
