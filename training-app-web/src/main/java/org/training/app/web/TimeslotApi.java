48d74b8915b99c343b0cd32ad9b779bf2c3bbec1
lucicmarko93 <marko.lucic@netsetglobal.rs>
2020-05-15 11:12:41 +0200

    fix for api endpoint

--- training-app-web/src/main/java/org/training/app/web/TimeslotApi.java
+++ training-app-web/src/main/java/org/training/app/web/TimeslotApi.java
@@ -1,15 +1,14 @@
 package org.training.app.web;
 
 import javax.ejb.EJB;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.training.app.core.TimeslotBean;
 import org.training.app.core.data.TimeslotRequest;
-import org.training.app.core.data.TimeslotResponse;
 
 import lombok.NoArgsConstructor;
 
@@ -20,11 +19,13 @@ public class TimeslotApi {
 	@EJB
 	private TimeslotBean timeslotBean;
 	
-	@GET
+	@POST
 	@Path("timeslots")
 	@Produces(MediaType.APPLICATION_JSON)
-	public TimeslotResponse processRequest(TimeslotRequest timeslotRequest) {		
-		return timeslotBean.process(timeslotRequest);
+	public Response processRequest(TimeslotRequest timeslotRequest) {		
+		return Response.status(Response.Status.OK)
+				.entity(timeslotBean.process(timeslotRequest))
+				.build();
 	}
 
 }
